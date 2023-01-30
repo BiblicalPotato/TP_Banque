c94b25c9d3bf5531e99b33bccba4824f8224ac5f
yohann <kurasiak.yoh.pat.ger@outlook.fr>
2023-01-30 15:51:42 +0100

    creation imprimer infos client (elle devra etre ameliorer)

--- src/Client.java
+++ src/Client.java
@@ -1,90 +1,112 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Client {
 
-	String id;
-	String nom;
-	String prenom;
-	String naissance;
-	String email;
+    String id;
+    String nom;
+    String prenom;
+    String naissance;
+    String email;
 
-	ArrayList<Compte> listeCompteClient = new ArrayList<Compte>();
+    ArrayList<Compte> listeCompteClient = new ArrayList<Compte>();
 
-	public Client(String id, String nom, String prenom, String naissance, String email) {
+    public Client(String id, String nom, String prenom, String naissance, String email) {
 
-		this.id = id;
-		this.nom = nom;
-		this.prenom = prenom;
-		this.naissance = naissance;
-		this.email = email;
+	this.id = id;
+	this.nom = nom;
+	this.prenom = prenom;
+	this.naissance = naissance;
+	this.email = email;
 
+    }
+
+    public String getEmail() {
+	return email;
+    }
+
+    public void setEmail(String email) {
+	this.email = email;
+    }
+
+    public ArrayList<Compte> getListeCompteClient() {
+	return listeCompteClient;
+    }
+
+    public void setListeCompteClient(ArrayList<Compte> listeCompteClient) {
+	this.listeCompteClient = listeCompteClient;
+    }
+
+    public String getId() {
+	return id;
+    }
+
+    public void setId(String id) {
+	this.id = id;
+    }
+
+    public String getNom() {
+	return nom;
+    }
+
+    public void setNom(String nom) {
+	this.nom = nom;
+    }
+
+    public String getPrenom() {
+	return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+	this.prenom = prenom;
+    }
+
+    public String getNaissance() {
+	return naissance;
+    }
+
+    public void setNaissance(String naissance) {
+	this.naissance = naissance;
+    }
+
+    public void afficherListeComptes(String id) {
+	System.out.println("___________________________________________________________________");
+	System.out.println("Liste de compte");
+	System.out.println("___________________________________________________________________");
+	System.out.println("Numéro de compte				Solde");
+	for (Compte compte : listeCompteClient) {
+	    if (id.equals(this.id)) {
+		System.out.println(compte + "					" + compte.getSolde()
+			+ "				");
+	    }
 	}
+	return;
+    }
 
-	public String getEmail() {
-		return email;
+    public void afficherNoCompte() {
+	for (Compte compte : listeCompteClient) {
+	    System.out.print(compte.getNoCompte());
 	}
+	return;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void imprimerInfosClient() {
+	try {
+	    FileWriter fw = new FileWriter("Fiche client.txt");
 
-	public ArrayList<Compte> getListeCompteClient() {
-		return listeCompteClient;
-	}
+	    fw.write("Fiche client " + "\r\n");
+	    fw.write(id + "\r\n");
+	    fw.write(nom + "\r\n");
+	    fw.write(prenom + "\r\n");
+	    fw.write(naissance + "\r\n" + "\r\n");
+	    fw.write("Listte de compte" + "\r\n");
+	    fw.write("Numéro de compte		Solde		" + "\r\n");
 
-	public void setListeCompteClient(ArrayList<Compte> listeCompteClient) {
-		this.listeCompteClient = listeCompteClient;
-	}
+	    fw.close();
 
-	public String getId() {
-		return id;
+	} catch (IOException e) {
+	    e.printStackTrace();
 	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getNom() {
-		return nom;
-	}
-
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-
-	public String getPrenom() {
-		return prenom;
-	}
-
-	public void setPrenom(String prenom) {
-		this.prenom = prenom;
-	}
-
-	public String getNaissance() {
-		return naissance;
-	}
-
-	public void setNaissance(String naissance) {
-		this.naissance = naissance;
-	}
-
-	public void afficherListeComptes(String id) {
-		System.out.println("___________________________________________________________________");
-		System.out.println("Liste de compte");
-		System.out.println("___________________________________________________________________");
-		System.out.println("Numéro de compte				Solde");
-		for (Compte compte : listeCompteClient) {
-			if (id.equals(this.id)) {
-				System.out.println(compte + "					" + compte.getSolde() + "				");
-			}
-		}
-		return;
-	}
-
-	public void afficherNoCompte() {
-		for (Compte compte : listeCompteClient) {
-			System.out.print(compte.getNoCompte());
-		}
-		return;
-	}
+    }
 }
